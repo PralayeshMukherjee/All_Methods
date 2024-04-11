45ffcf98e52a9ff55c5320029406d179d05e1602
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2024-04-11 22:16:05 +0530

    Add all basic method about data type,array,string

---
+++ method.java
@@ -0,0 +1,65 @@
+public class method {
+
+    public static void main(String[] args) {
+        /*
+         Data types
+
+         int :- to store integer
+         char:- to store character => ''
+         float:- to store floating value
+         double:- to store double value
+         long:- to store long integer
+         String:- to store string data type => ""
+
+         Array
+
+         init => i) int arr[] = new int[size];
+                 ii) int arr[] = {1,2,3,4,5};
+         fetch => arr[i];
+         print => System.out.println(Arrays.toString(arr));
+
+         String 
+         
+         init => String str = "ABC";
+         methods:=>
+
+                  i) charAt(): - Returns the character at the specified index in the string.
+                  ii) compareTo(): - Compares two strings lexicographically.
+                  iii)concat(): - Concatenates two strings.
+                  iv)contains(): - Checks if the string contains a specified sequence of characters.
+                  v)endsWith(): - Checks if the string ends with a specified sequence of characters.
+                  vi)equals(): - Compares two strings for equality.
+                  vii)equalsIgnoreCase(): - Compares two strings for equality, ignoring case.
+                  viii)indexOf(): - Returns the index of the first occurrence of a specified character or sequence of characters in the string.
+                  ix)lastIndexOf(): - Returns the index of the last occurrence of a specified character or sequence of characters in the string.
+                  x)length(): - Returns the length of the string.
+                  xi)replace(): - Replaces all occurrences of a specified character or sequence of characters in the string with another character or sequence of characters.
+                  xii)split(): - Splits the string into an array of strings, using a specified delimiter.
+                  xiii)startsWith(): - Checks if the string starts with a specified sequence of characters.
+                  xiv)substring(): - Returns a substring of the string, starting at a specified index and ending at another specified index.
+                  xv)toLowerCase(): - Converts the string to lowercase.
+                  xvi)toUpperCase(): - Converts the string to uppercase.
+                  xvii)trim(): - Removes leading and trailing whitespace from the string.
+
+         transformation:- i) Integer to String =>
+                                a) int i = 123;
+                                   String str = Integer.toString(i);
+                                b) int i = 123;
+                                   String str = String.valueOf(i);
+                                c) int i = 123;
+                                   String str = i + "";
+                          ii) String to Integer
+                                a) String str = "123";
+                                   int i = Integer.parseInt(str);
+                                b) String str = "123";
+                                   Integer i = Integer.valueOf(str);
+                          iii) Integer to Character =>
+                                a) char c = (char) 65;
+                          iv)  Character to Integer =>
+                                a) char ch = '5';
+                                   int i = ch - '0';
+                                b) char ch = '5';
+                                   int i = Integer.parseInt(String.valueOf(ch));
+         */
+    }
+}
